7847a9f5bdd7e1ed5af026d3bc42e978864b02dd
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-09-21 15:03:35 -0400

    Improper updates in recycler view, text size incorrectly set in null condition

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 
 class TextSizeAdapter(private val context: Context, private val textSizes: Array<Int>) : BaseAdapter() {
 
+
     override fun getCount(): Int {
         return textSizes.size
     }
@@ -22,10 +23,21 @@ class TextSizeAdapter(private val context: Context, private val textSizes: Array
     }
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        val textView = TextView(context)
+        val textView : TextView
+
+        if (p1 != null) {
+            textView = p1 as TextView
+        } else {
+            textView = TextView(context)
+            textView.setPadding(5,10,0,10)
+        }
+
         textView.text = textSizes[p0].toString()
         textView.textSize = textSizes[p0].toFloat()
-        textView.setPadding(5,10,0,10)
         return textView
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return super.getDropDownView(position, convertView, parent)
     }
 }
